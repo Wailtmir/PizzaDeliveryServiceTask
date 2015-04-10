5558483f113ad4da256a725f3cec83bf9238b153
Vitaliy Miroshnychenko <ya14miroshnychenko@gmail.com>
2015-04-10 11:18:34 +0300

    Mir
    Home

---
+++ src/main/java/ua/shad/yandex/pizzadelivery/service/PizzaService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.shad.yandex.pizzadelivery.service;
+
+import ua.shad.yandex.pizzadelivery.domain.Pizza;
+import java.util.List;
+
+/**
+ *
+ * @author Alexandr
+ */
+public interface PizzaService {
+
+    List<Pizza> getAllPizzas();
+}
